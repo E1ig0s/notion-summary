400e8beb401ed87273c7a430f4c9c19e65cf968d
E1ig0s <E1ig0sAnonymous777@gmail.com>
2024-03-31 21:52:55 +0900

    feat : 사진을 찍고 텍스트를 검출하여 화면에 띄워주는 기능 추가 1. CemeraX를 사용하여 사진을 찍고 저장. 2. 저장한 사진을 불러와 텍스트를 검출한 뒤 앱 화면에 표시.

--- app/src/main/java/com/example/notion_summary/MainActivity.kt
+++ app/src/main/java/com/example/notion_summary/MainActivity.kt
@@ -1,17 +1,268 @@
 package com.example.notion_summary
 
-import androidx.appcompat.app.AppCompatActivity
+
+
+import android.Manifest
+import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.ImageCapture
+import androidx.camera.video.Recorder
+import androidx.camera.video.Recording
+import androidx.camera.video.VideoCapture
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import android.widget.Toast
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.core.Preview
+import androidx.camera.core.CameraSelector
 import android.util.Log
-import org.opencv.android.OpenCVLoader
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
+import androidx.camera.video.FallbackStrategy
+import androidx.camera.video.MediaStoreOutputOptions
+import androidx.camera.video.Quality
+import androidx.camera.video.QualitySelector
+import androidx.camera.video.VideoRecordEvent
+import androidx.core.content.PermissionChecker
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.example.notion_summary.databinding.ActivityMainBinding
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.korean.KoreanTextRecognizerOptions
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import java.io.File
+import java.io.IOException
+import java.nio.ByteBuffer
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+typealias LumaListener = (luma: Double) -> Unit
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityMainBinding
+
+    private var imageCapture: ImageCapture? = null
+
+    private var videoCapture: VideoCapture<Recorder>? = null
+    private var recording: Recording? = null
+
+    private lateinit var cameraExecutor: ExecutorService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        OpenCVLoader.initDebug()
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
 
-        Log.d("Test", OpenCVLoader.OPENCV_VERSION);
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
 
+        // Set up the listeners for take photo and video capture buttons
+        viewBinding.imageCaptureButton.setOnClickListener { takePhoto() }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create time stamped name and MediaStore entry.
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+            }
+        }
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues)
+            .build()
+
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults){
+                    val savedUri = output.savedUri ?: return // 저장된 URI 획득, null이면 반환
+
+                    val msg = "Photo capture succeeded: $savedUri"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+
+                    recognizeTextFromImage(savedUri)
+
+                    // 메인 스레드에서 ImageView에 이미지를 설정
+//                    runOnUiThread {
+//                        val imageView = viewBinding.imageView // 레이아웃에 따라 실제 ID를 사용하세요.
+//                        // savedUri 값 로그에 출력
+//                        Log.d(TAG, "loading image from URI: $savedUri")
+//                        Glide.with(this@MainActivity)
+//                            .load(savedUri)
+//                            .listener(object : RequestListener<Drawable> {  // Drawable 타입을 명시
+//                                override fun onLoadFailed(e: GlideException?, model: Any?, target: com.bumptech.glide.request.target.Target<Drawable>?, isFirstResource: Boolean): Boolean {
+//                                    // 이미지 로딩 실패 로그 출력
+//                                    Log.e(TAG, "이미지 로딩 실패: URI: $savedUri", e)
+//                                    return false  // 로딩 실패 시 false 반환
+//                                }
+//
+//                                override fun onResourceReady(resource: Drawable?, model: Any?, target: com.bumptech.glide.request.target.Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+//                                    // 이미지 로딩 성공 로그 출력
+//                                    Log.d(TAG, "이미지 로딩 성공: URI: $savedUri")
+//                                    return false  // 리소스 준비 완료 시 false 반환
+//                                }
+//                            })
+//                            .into(imageView)
+//                    }
+
+                }
+            }
+        )
+    }
+
+    private fun checkAndDownloadModelIfNeeded() {
+        val modelFile = File(getFilesDir(), "mlkit-ocr-models/tflite_langid.tflite")
+        if (!modelFile.exists()) {
+            // 모델 파일이 없다면 다운로드 로직을 여기에 구현합니다.
+            // 예: 서버에서 모델 파일을 다운로드하고, 위의 경로에 저장합니다.
+        }
+    }
+
+    // 2. 텍스트 인식 기능 구현
+    private fun recognizeTextFromImage(imageUri: Uri) {
+        // 모델 파일 확인 및 필요 시 다운로드
+        checkAndDownloadModelIfNeeded()
+
+        val image = InputImage.fromFilePath(this, imageUri)
+
+        // 한국어 텍스트 인식 옵션을 사용하여 인스턴스 생성
+        val recognizer = TextRecognition.getClient(KoreanTextRecognizerOptions.Builder().build())
+
+        recognizer.process(image)
+            .addOnSuccessListener { visionText ->
+                // 텍스트 인식 성공
+                val resultText = visionText.text
+                // Intent 사용하여 새 Activity로 이동
+                val intent = Intent(this, TextDisplayActivity::class.java).apply {
+                    putExtra("recognized_text", resultText)
+                }
+                startActivity(intent)
+            }
+            .addOnFailureListener { e ->
+                // 텍스트 인식 실패
+                Log.e(TAG, "Text recognition error: ${e.message}", e)
+            }
+    }
+
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder()
+                .build()
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "NotionSummary"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
     }
 }
